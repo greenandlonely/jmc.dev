81110d9d5851e0503cb315cb45a82d6734a93820
greenandlonely <jclarke0206@gmail.com>
2024-11-25 12:13:38 +0000

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,22 @@
 export default function (eleventyConfig) {
-  eleventyConfig.setBrowserSyncConfig({
-    notify: true,
-    files: "./_site/css/**/*.css",
-  });
+  // Order matters, put this at the top of your configuration file.
+  // This is relative to your input directory!
+  eleventyConfig.setLayoutsDirectory("_layouts");
+
+  // Copy the `images/` & `fonts/` directories
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("js");
+
+  return {
+    passthroughFileCopy: true,
+  };
 }
+
+export const config = {
+  dir: {
+    // These are both relative to your input directory!
+    includes: "_includes",
+    layouts: "_layouts",
+  },
+};
