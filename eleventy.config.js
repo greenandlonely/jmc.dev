17dfbb59ccc67ddb9ef19002a8e41b7d3b7f7503
greenandlonely <jclarke0206@gmail.com>
2025-01-17 19:13:28 +0000

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,7 @@
 export default function (eleventyConfig) {
-  // Order matters, put this at the top of your configuration file.
-  // This is relative to your input directory!
   eleventyConfig.setLayoutsDirectory("_layouts");
 
-  // Copy the `images/` & `fonts/` directories
+  // Copy the `images/`, `fonts/` & `js/` directories
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("js");
@@ -15,7 +13,6 @@ export default function (eleventyConfig) {
 
 export const config = {
   dir: {
-    // These are both relative to your input directory!
     includes: "_includes",
     layouts: "_layouts",
   },
