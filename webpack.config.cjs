5088e3a627cde4f2956115c76969743df00b498e
greenandlonely <jclarke0206@gmail.com>
2025-01-17 19:13:10 +0000

    Update webpack.config.cjs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: ["./js/viewport-height.js", "./js/aos.js"],
+  entry: ["./js/viewport-height.js"],
   output: {
     path: path.resolve(__dirname, "js"),
     filename: "bundle.js",
